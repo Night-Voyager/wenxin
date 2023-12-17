ddbc72418ca157b8cfd427bbead585940f34936f
Night-Voyager <haoyukun@bjtu.edu.cn>
2023-12-18 00:13:05 +0800

    feat: cover upload icon

--- src/components/TheView.vue
+++ src/components/TheView.vue
@@ -3,17 +3,23 @@
         <el-row>
             <el-col :span="12">
                 <el-upload
-                    ref="uploadRef"
+                    v-model:file-list="fileList"
                     :limit="1"
                     :on-exceed="handleExceed"
+                    :on-change="handleChange"
                     :auto-upload="false"
                     drag
                 >
-                    <el-icon class="el-icon--upload">
-                        <upload-filled />
-                    </el-icon>
-                    <div class="el-upload__text">
-                        拖动文件到此处，或<em>点击上传</em>
+                    <div v-if="fileList.length == 0">
+                        <el-icon class="el-icon--upload">
+                            <upload-filled />
+                        </el-icon>
+                        <div class="el-upload__text">
+                            拖动文件到此处，或<em>点击上传</em>
+                        </div>
+                    </div>
+                    <div v-else>
+                        <el-image :src="fileUrl" />
                     </div>
                 </el-upload>
             </el-col>
@@ -28,15 +34,21 @@
 <script setup lang="ts">
 import {UploadFilled} from "@element-plus/icons-vue";
 import {ref} from "vue";
-import {genFileId, UploadInstance, UploadProps, UploadRawFile} from "element-plus";
+import {genFileId, UploadUserFile, UploadProps, UploadRawFile} from "element-plus";
 
-const uploadRef = ref<UploadInstance>();
+const fileList = ref<UploadUserFile[]>([]);
+const fileUrl = ref('');
 
 const handleExceed: UploadProps['onExceed'] = (files) => {
-    uploadRef.value!.clearFiles();
+    fileList.value = [];
     const file = files[0] as UploadRawFile;
     file.uid = genFileId();
-    uploadRef.value!.handleStart(file);
+    fileList.value.push(file);
+    fileUrl.value = URL.createObjectURL(file);
+};
+
+const handleChange: UploadProps['onChange'] = (uploadFile) => {
+    fileUrl.value = URL.createObjectURL(uploadFile.raw);
 };
 </script>
 
